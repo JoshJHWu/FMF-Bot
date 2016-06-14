803b6182205fc1adbfd82361459ab2674c203343
Josh Wu <joshua.jh.wu@gmail.com>
2016-06-14 16:03:09 -0400

    add encapsulation of submission objects

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import praw
 from submission import Submission
+from container import Container
 
 user_agent = ("FMFA 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
 subreddit = r.get_subreddit("frugalmalefashion")
+c = Container()
 
 for submission in subreddit.get_new(limit = 5):
-    print "Title: ", submission.title
-    print "Url: ", submission.url
-    print "---------------------------------\n"
+    s = Submission({'title':submission.title,'url':submission.url})
+    c.contents.append(s)
+    # print "Title: ", s.title
+    # print "Url: ", s.url
+    # print "---------------------------------\n"
+
+print c.contents[0].title
--- submission.py
+++ submission.py
@@ -3,3 +3,5 @@ class Submission():
     def __init__(self, args = {}):
         self.title = args['title']
         self.url = args['url']
+
+# print Submission({'title':"title", 'url':1}).url
