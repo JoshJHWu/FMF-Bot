221b0ee99e9e01f24452e8bc5cb196b6d1965285
joshjhwu <joshwu@gethealthie.com>
2016-08-04 23:17:11 -0400

    add mailer

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
+import smtplib
 import praw
 from submission import Submission
 
+#add key word
+keyword = "cotton"
+
+# reddit parser
 user_agent = ("FMFA 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
 subreddit = r.get_subreddit("frugalmalefashion")
 
+key_submissions = []
+
 for submission in subreddit.get_new(limit = 5):
-    print "Title: ", submission.title
-    print "Url: ", submission.url
-    print "---------------------------------\n"
+    for word in submission.title.lower().split():
+        if word == keyword:
+            key_submissions.append(submission)
+            print "SENDING:"
+            print "Title: ", submission.title
+            print "Url: ", submission.url
+            print "---------------------------------\n"
+
+            # mailer
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("fmfbot@gmail.com", "NachoMontreal")
+
+            msg = "Howdy, Your keyword: %s found a match! \n\n Title: %s \n Url: %s" % (keyword, submission.title, submission.url)
+            server.sendmail("fmfbot@gmail.com", "joshua.jh.wu@gmail.com", msg)
+            server.quit()
