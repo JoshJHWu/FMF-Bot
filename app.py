8a0be21e6ca671d978967522c797ea272af21523
joshjhwu <joshwu@gethealthie.com>
2016-08-06 18:00:41 -0400

    add loop

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import smtplib
 import praw
+import time
 from submission import Submission
 
 #add key word
-keyword = "cotton"
+keyword = "common"
 
 # reddit parser
-user_agent = ("FMFA 0.1")
+user_agent = ("UNIQUE ID")
 
 r = praw.Reddit(user_agent = user_agent)
 
@@ -14,20 +15,26 @@ subreddit = r.get_subreddit("frugalmalefashion")
 
 key_submissions = []
 
-for submission in subreddit.get_new(limit = 5):
-    for word in submission.title.lower().split():
-        if word == keyword:
-            key_submissions.append(submission)
-            print "SENDING:"
-            print "Title: ", submission.title
-            print "Url: ", submission.url
-            print "---------------------------------\n"
+def search_reddit():
+    for submission in subreddit.get_new(limit = 5):
+        for word in submission.title.lower().split():
+            if word == keyword:
+                key_submissions.append(submission)
+                print "SENDING:"
+                print "Title: ", submission.title
+                print "Url: ", submission.url
+                print "---------------------------------\n"
 
-            # mailer
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login("fmfbot@gmail.com", "NachoMontreal")
+                # mailer
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                server.login("BOT EMAIL", "PASS")
 
-            msg = "Howdy, Your keyword: %s found a match! \n\n Title: %s \n Url: %s" % (keyword, submission.title, submission.url)
-            server.sendmail("fmfbot@gmail.com", "joshua.jh.wu@gmail.com", msg)
-            server.quit()
+                msg = "Howdy, Your keyword: %s found a match! \n\n Title: %s \n Url: %s" % (keyword, submission.title, submission.url)
+                server.sendmail("BOT EMAIL", "RECIPIENT EMAIL", msg)
+                server.quit()
+
+while True:
+    search_reddit()
+    print "working"
+    time.sleep(60)
